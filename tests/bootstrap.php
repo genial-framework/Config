3f72cc9b1801c603f422ad924902343212fc9acf
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-21 23:10:17 -0500

    Create bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Config> for the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Config/blob/master/LICENSE> New BSD License.
+ */
+require 'vendor/autoload.php';
