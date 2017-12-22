4edf29df24885d1e27f1695307090ac44229ceb7
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-21 23:07:35 -0500

    Update ExceptionInterface.php

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,1 +1,21 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Core> for the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Core/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Config\Exception;
+
+/**
+ * ExceptionInterface.
+ */
+interface ExceptionInterface
+{
+}
