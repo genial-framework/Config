763396d43187e8555a8b466f2cf07899158f2019
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-01 01:08:40 -0500

    Update Session.php

--- src/Dependent/Session.php
+++ src/Dependent/Session.php
@@ -12,11 +12,34 @@
 
 namespace Genial\Config\Dependent;
 
+use Genial\Config\Exception;
+
 /**
  * Session.
  */
 class Session implements DependentInterface, SessionDependentInterface
 {
+    
+    /**
+     * __construct().
+     *
+     * Check to see if the env function exists.
+     *
+     * @throws RuntimeException If the env function does not exist.
+     *
+     * @return void.
+     */
+    function __construct()
+    {
+        if (!function_exists('env'))
+        {
+            throw new Exception\RuntimeException(sprintf(
+                '`%s` The `env()` function does not exist.',
+                __METHOD__
+            ));
+        }
+        $this->config = env('session');
+    }
     
     /**
      * run().
@@ -27,7 +50,35 @@ class Session implements DependentInterface, SessionDependentInterface
      */
     public function run()
     {
-      
+        if (isset($this->config['session_name']))
+        {
+            session_name($this->config['session_name']);   
+        }
+        if (isset($this->config['session_encrypt'])
+        {
+            $encrypt = true;
+        }
+        if ()
+        {
+                
+        }
+        
+        if ($this->config['session_handler'] == 'files'))
+        {
+            ini_set('session.save_path', APP_ROOT . '/temp');
+            if (version_compare(PHP_VERSION, '7.2.0') >= 0)
+            {
+                session_gc();
+            } else
+            {
+                ini_set('session.gc_maxlifetime', 0);
+            }
+        } elseif (defined('DATABASE_DEPENDENT_ACTIVE') && $this->config['session_handler'] == 'db')
+        {
+            $table = env('database', 'table_prefix', 'genial') . '_session';
+            /* ... */
+        }
+        
     }
 
 }
